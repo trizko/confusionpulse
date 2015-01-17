c005d9d513e962337f6798149af7f24f99c758bd
Tony Rizko <tony@rizkocircle.com>
2015-01-16 17:14:18 -0800

    refactors code

--- client/App/Teacher/teacherCtrl.js
+++ client/App/Teacher/teacherCtrl.js
@@ -3,9 +3,8 @@ angular
 	  .module('TeacherController', [])
 	  .controller('TeacherController', ['$scope', 'teacherFactory', function($scope, teacherFactory){
 
-			$scope.confusedStudents = teacherFactory.confusedStudents;
       $scope.numStudents = 0;
-			
+
       setInterval(function () {
         $scope.numStudents = teacherFactory.getCount();
         $scope.$apply();
--- client/App/Teacher/teacherFactory.js
+++ client/App/Teacher/teacherFactory.js
@@ -4,7 +4,6 @@ angular
 
 		var socket = io();
 
-		var confusedStudents = [];
 		var uniqueStudents = [];
 		var getCount = function () {
 			return uniqueStudents.length;
@@ -13,15 +12,13 @@ angular
 		//listens for any updates and will call a function in the teacher.js
 		//will also console.log the name from the student object that was submitted
 		socket.on("teacher:update", function(data){
-		    confusedStudents.push(data);
-		    calculateConfusion(confusedStudents);
+		    calculateConfusion(data);
 		    if (_.indexOf(uniqueStudents, data.studentID) === -1) {
 		    	uniqueStudents.push(data.studentID);
 		    }
 		})
 
 		return {
-			confusedStudents: confusedStudents,
 			getCount: getCount
 		};
 
